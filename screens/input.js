e02f75058596f446bb524cd5cb7e6e1b6b113e4f
rliu23 <rebecca.liu.1c@gmail.com>
2024-04-25 14:40:10 -0500

    BottomNavigator + InputsScreen

--- screens/input.js
+++ screens/input.js
@@ -3,30 +3,15 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import InputScreen from './screens/input';
 
 const Tab = createBottomTabNavigator();
 export default function App() {
   return (
-    <NavigationContainer>
-    <Tab.Navigator>
-    <Tab.Screen name="Home Screen" component={HomeScreen}/>
-    <Tab.Screen name="Input" component={InputScreen}/>
-    
-    </Tab.Navigator>
-    </NavigationContainer>
-  );
-}
-
-function HomeScreen() {
-  return (
     <View style={styles.container}>
-      <Text>THIS IS A TEST!</Text>
+      <Text>Input!</Text>
       <StatusBar style="auto" />
     </View>
-
-  )
-
+  );
 }
 
 const styles = StyleSheet.create({
