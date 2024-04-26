6690c0809d5cdb085ced9c9174b97a1216b6cbd5
rliu23 <rebecca.liu.1c@gmail.com>
2024-04-26 05:33:31 -0500

    input fields (broken)

--- App.js
+++ App.js
@@ -3,17 +3,40 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import InputScreen from './screens/input';
+import { Feather } from '@expo/vector-icons';
 
 const Tab = createBottomTabNavigator();
+const Stack = createNativeStackNavigator();
+function Screens() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Home" 
+        component={HomeScreen}
+        options={{
+          tabBarIcon: ({ color, size }) => 
+            (<Feather name="home" color={color} size={size} />),
+          }}
+      />
+      <Tab.Screen name="Input" component={InputScreen}
+        options={{
+          tabBarIcon: ({ color, size }) => 
+          (<Feather name="edit-3" color={color} size={size} />),
+        }}
+      />
+    </Tab.Navigator>
+
+      )
+}
+
+
 export default function App() {
   return (
     <NavigationContainer>
-    <Tab.Navigator>
-    <Tab.Screen name="Home Screen" component={HomeScreen}/>
-    <Tab.Screen name="Input" component={InputScreen}/>
-    
-    </Tab.Navigator>
+    <Stack.Navigator>
+      <Stack.Screen name="Container" component={Screens} options={{headerShown: false}}/>
+    </Stack.Navigator>
     </NavigationContainer>
   );
 }
@@ -28,6 +51,7 @@ function HomeScreen() {
   )
 
 }
+
 
 const styles = StyleSheet.create({
   container: {
--- screens/input.js
+++ screens/input.js
@@ -1,24 +1,143 @@
-import * as React from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import React, { useState } from 'react';
+import { Text, TouchableOpacity, StyleSheet, View, TextInput, Button, Switch, Touchable } from 'react-native';
 
-const Tab = createBottomTabNavigator();
-export default function App() {
+const FlexibleInputFields = ({  }) => {
+  const [fields, setFields] = useState([{ id: Date.now(), title: '', value: '', isFixed: false }]); // State to hold text field values
+
+  const addTextField = () => {
+    setFields([...fields, { id: Date.now(), title: '', value: '', isFixed: false }]);
+  };
+
+   const handleTitleChange = (text, id) => {
+    const newFields = fields.map(field => {
+      if (field.id === id) {
+        return { ...field, title: text };
+      }
+      return field;
+    });
+    setFields(newFields);
+  };
+
+  const handleValueChange = (text, id) => {
+    const newFields = fields.map(field => {
+      if (field.id === id) {
+        return { ...field, value: text };
+      }
+      return field;
+    });
+    setFields(newFields);
+  };
+
+  const handleIsFixedChange = (value, id) => {
+    const newFields = fields.map(field => {
+      if (field.id === id) {
+        return { ...field, isFixed: JSON.parse(value) };
+      }
+      return field;
+    });
+    setFields(newFields);
+  };
+
+  const removeTextField = (id) => {
+    const newFields = fields.filter(field => field.id !== id);
+    setFields(newFields);
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Input!</Text>
-      <StatusBar style="auto" />
+    <View >
+      {fields.map((field, index) => (
+        <View key={index}>
+        <View style={styles.container}>
+          <View style={styles.inputContainer}>
+            <TextInput
+              value={field.title}
+              onChangeText={(text) => handleTitleChange(text, index)}
+              placeholder="Category"
+              style={styles.input}
+            />
+          </View>
+          <View style={styles.inputContainer}>
+            <TextInput
+              value={field.value}
+              onChangeText={(text) => handleValueChange(text, index)}
+              placeholder="Amount"
+              style={styles.input}
+            />
+          </View>
+          <View style={styles.inputContainer}>
+            <Switch
+              value={field.isFixed}
+              onValueChange={(value) => handleIsFixedChange(value, index)}
+            />
+          </View>
+          <TouchableOpacity
+            style={styles.removeButton}
+            onPress={() => removeTextField(field.id)}
+            underlayColor='white'>
+            <Text style={styles.removeText}>x</Text>
+          </TouchableOpacity>
+        </View>
+        </View>
+        
+      ))}
+      <TouchableOpacity
+        style={styles.addButton}
+        onPress={addTextField}
+        underlayColor='white'>
+        <Text style={styles.addText}>Add Category</Text>
+      </TouchableOpacity>
+      
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    backgroundColor: 'white',
+    flexDirection: 'row',
     justifyContent: 'center',
+    alignItems: 'center',
   },
+  inputContainer: {
+    marginVertical: 10,
+    marginHorizontal: 10,
+    flex: 1,
+  },
+  input: {
+    fontSize: 20,
+    height: 40,
+    borderColor: 'rgba(200,200,200,5)',
+    borderWidth: 1,
+    paddingHorizontal: 5,
+    borderRadius: 4,
+  },
+  addText: {
+    fontSize: 16,
+    color: 'white',
+    textAlign: 'center',
+    paddingVertical: 5,
+  },
+  addButton: {
+    margin: 10,
+    alignSelf: 'center',
+    backgroundColor: 'dodgerblue',
+    borderRadius: 15,
+    width: '30%',
+  },
+  removeText: {
+    fontSize: 15,
+    color: 'white',
+    textAlign: 'center',
+    paddingVertical: 1,
+
+  },
+  removeButton: {
+    margin: 10,
+    alignSelf: 'center',
+    backgroundColor: 'rgba(255,0,0,.8)',
+    borderRadius: 22,
+    flex: 0.22,
+  }
 });
+
+export default FlexibleInputFields;
